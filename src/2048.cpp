e9adfd33106b35dd2052caded7e98ff5a2553ad7
Tushar Maheshwari <tushar27192@gmail.com>
2021-09-25 14:53:47 +0530

    Movable tiles for 2048

---
+++ src/2048.cpp
@@ -0,0 +1,114 @@
+// Copyright 2021 Tushar Maheshwari. All rights reserved.
+// Use of this source code is governed by the MIT license that can be found in
+// the LICENSE file.
+
+#include <algorithm> // for find, shuffle
+#include <array>     // for array
+#include <cstddef>   // for size_t
+#include <memory>    // for shared_ptr
+#include <random>    // for random_device, mt19937
+#include <string>    // for to_string, string
+#include <utility>   // for move, swap
+
+#include <ftxui/component/component.hpp>          // for CatchEvent, Renderer
+#include <ftxui/component/event.hpp>              // for Event
+#include <ftxui/component/screen_interactive.hpp> // for ScreenInteractive
+#include <ftxui/dom/elements.hpp> // for text, hbox, vbox, window, Element, Elements
+
+template <typename Element, size_t Height, size_t Width = Height> class Grid {
+public:
+  constexpr static auto height = Height;
+  constexpr static auto width = Width;
+  constexpr static auto MaximumNumber = height * width;
+  std::array<Element, MaximumNumber> elements;
+  std::mt19937 prng;
+
+  Grid() : elements{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}, prng{std::random_device{}()} {
+    std::shuffle(elements.begin(), elements.end(), prng);
+  }
+
+  size_t DigitsInMaximumNumber() const {
+    auto number = 1 << *std::max_element(elements.begin(), elements.end());
+    size_t ret = 1;
+    while (number /= 10)
+      ++ret;
+    return ret;
+  }
+
+  void move(int x, int y) {
+    assert(std::abs(x) + std::abs(y) == 1);
+    const auto outer_end = std::abs(x) == 1 ? height : width;
+    const auto direction = std::abs(x) == 1 ? x : y;
+    const auto inner_begin = direction == 1 ? 0 : (height + width - outer_end) - 1;
+    const auto inner_end = inner_begin + direction * (height + width - outer_end);
+    for (int outer = 0; outer != outer_end; ++outer) {
+      for (int inner = inner_begin, write = inner_begin; inner != inner_end; inner += direction) {
+        if (x != 0) {
+          if (at(outer, inner) != 0) {
+            std::swap(at(outer, write), at(outer, inner));
+            write += direction;
+          }
+        } else {
+          if (at(inner, outer) != 0) {
+            std::swap(at(write, outer), at(inner, outer));
+            write += direction;
+          }
+        }
+      }
+    }
+  }
+
+private:
+  Element& at(int row, int col) {
+    return elements[row * width + col];
+  }
+};
+
+using namespace ftxui;
+
+int main(int argc, const char *argv[]) {
+  auto screen = ScreenInteractive::FitComponent();
+  using State = Grid<int, 4>;
+  State state;
+
+  auto component = Renderer([&state] {
+    Elements children;
+    Elements row;
+    const auto DigitsInMaximumVisibleNumber = state.DigitsInMaximumNumber();
+    for (auto cell : state.elements) {
+      if (cell == 0) {
+        row.push_back(text("") |
+                      size(WIDTH, EQUAL, DigitsInMaximumVisibleNumber) |
+                      border);
+      } else {
+        row.push_back(text(std::to_string(1 << cell)) | align_right |
+                      size(WIDTH, EQUAL, DigitsInMaximumVisibleNumber) |
+                      border);
+      }
+      if (row.size() == State::width) {
+        children.push_back(hbox(std::move(row)));
+        row.clear();
+      }
+    }
+    return window(text("1 << 11"), vbox(std::move(children)));
+  });
+
+  component = CatchEvent(component, [&](Event event) {
+    if (event == Event::Escape || event.input() == "q") {
+      screen.ExitLoopClosure()();
+    } else if (event == Event::Return || event.input() == "r") {
+      state = {};
+    } else if (event == Event::ArrowUp) {
+      state.move(0, 1);
+    } else if (event == Event::ArrowDown) {
+      state.move(0, -1);
+    } else if (event == Event::ArrowLeft) {
+      state.move(1, 0);
+    } else if (event == Event::ArrowRight) {
+      state.move(-1, 0);
+    }
+    return false;
+  });
+
+  screen.Loop(component);
+}
